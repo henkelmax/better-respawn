7a8894d1a754943207283c3d377097b2df63cd21
Max Henkel <max@maxhenkel.de>
2023-04-19 08:55:08 +0200

    Update config builder
    (cherry picked from commit f7dfaea45aceb16099961b7c3f70e650629f54f7)

--- fabric/src/main/java/de/maxhenkel/betterrespawn/config/FabricServerConfig.java
+++ fabric/src/main/java/de/maxhenkel/betterrespawn/config/FabricServerConfig.java
@@ -5,9 +5,27 @@ import de.maxhenkel.configbuilder.ConfigBuilder;
 public class FabricServerConfig extends ServerConfig {
 
     public FabricServerConfig(ConfigBuilder builder) {
-        maxRespawnDistance = builder.integerEntry("max_respawn_distance", 256, 16, 8192);
-        minRespawnDistance = builder.integerEntry("min_respawn_distance", 128, 0, 4096);
-        respawnBlockRange = builder.integerEntry("respawn_block_range", 256, 0, Integer.MAX_VALUE);
+        maxRespawnDistance = builder.integerEntry(
+                "max_respawn_distance",
+                256,
+                16,
+                8192,
+                "The maximum distance to spawn the player away from its death location"
+        );
+        minRespawnDistance = builder.integerEntry(
+                "min_respawn_distance",
+                128,
+                0,
+                4096,
+                "The minimum distance to spawn the player away from its death location"
+        );
+        respawnBlockRange = builder.integerEntry(
+                "respawn_block_range",
+                256,
+                0,
+                Integer.MAX_VALUE,
+                "If the player is in this range of its bed/respawn anchor it will respawn there"
+        );
     }
 
 }
