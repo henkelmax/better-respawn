a6db12426c65aa7622052d7a1ad837089f37739f
Ivan Geevo <4586116+ivangeevo@users.noreply.github.com>
2023-12-20 17:51:41 +0200

    Change to hardcore respawn logic.// fixes

--- fabric/src/main/java/de/maxhenkel/betterrespawn/FabricBetterRespawnMod.java
+++ fabric/src/main/java/de/maxhenkel/betterrespawn/FabricBetterRespawnMod.java
@@ -1,8 +1,10 @@
 package de.maxhenkel.betterrespawn;
 
 import de.maxhenkel.betterrespawn.config.FabricServerConfig;
+import de.maxhenkel.betterrespawn.event.BiomeSpawnEvent;
 import de.maxhenkel.configbuilder.ConfigBuilder;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class FabricBetterRespawnMod extends BetterRespawnMod implements ModInitializer {
@@ -11,7 +13,15 @@ public class FabricBetterRespawnMod extends BetterRespawnMod implements ModIniti
     public void onInitialize() {
         init();
 
+
         SERVER_CONFIG = ConfigBuilder.build(FabricLoader.getInstance().getConfigDir().resolve(MODID).resolve("%s.properties".formatted(MODID)), true, FabricServerConfig::new);
+
+        // Register the event handler
+        ServerWorldEvents.LOAD.register(BiomeSpawnEvent::onWorldLoad);
+
+
+
     }
 
 }
+
