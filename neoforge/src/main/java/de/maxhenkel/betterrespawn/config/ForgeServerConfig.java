7bab44d56210b873bad39867ef77943ec8fdf4ca
Max Henkel <max@maxhenkel.de>
2023-12-21 16:45:09 +0100

    Use shared buildscript

--- neoforge/src/main/java/de/maxhenkel/betterrespawn/config/ForgeServerConfig.java
+++ neoforge/src/main/java/de/maxhenkel/betterrespawn/config/ForgeServerConfig.java
@@ -1,7 +1,7 @@
 package de.maxhenkel.betterrespawn.config;
 
 import de.maxhenkel.configbuilder.Config;
-import de.maxhenkel.configbuilder.ConfigEntry;
+import de.maxhenkel.configbuilder.entry.ConfigEntry;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class ForgeServerConfig extends ServerConfig {
